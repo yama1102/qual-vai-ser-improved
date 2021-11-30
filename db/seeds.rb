5731b8e922d0675deb291682f935b7e8d7e18475
Ana Westphalen <campos1984@hotmail.com>
2021-11-30 11:45:16 -0300

    seed de todos os models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+# User.destroy_all
+10.times do
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+end
+
+numbers = 0..3
+numbers.each do |i|
+  array_users = User.all
+  user = array_users[i]
+  array_users.slice(i)
+  group = Group.create!(user: user, name: Faker::Team.name)
+  Member.create!(user: user, group: group, adm: true)
+  numbers.each do |j|
+    member = array_users[j]
+    Member.create!(user: member, group: group)
+  end
+end
+
+Event.create!(
+  address: "Rua A, 3, bairro Santa Maria",
+  region: "Sudeste",
+  price: 200.00,
+  image: "https://static.cardkingdom.com/images/magic-the-gathering/dragons-maze/tithe-drinker-33674.jpg",
+  description: "All incluse, área Vip, bebida que pisca, balada eletrônica",
+  name: "Eletro Night",
+  date: Date.new(2021, 12, 31)
+)
+
+GroupedEvent.create!(group: Group.first, event: Event.first)
