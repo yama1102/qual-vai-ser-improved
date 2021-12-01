1ff14c320c7273c2d3a8fdea87b06003badfe20e
victorfonsecass <72159465+victorfonsecass@users.noreply.github.com>
2021-12-01 19:46:20 -0300

    Update profiles_controller.rb
    Apaguei comentário.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,15 +3,6 @@ class ProfilesController < ApplicationController
   def show
    @user = current_user
    @friendships = Friendship.where({ user_id: current_user.id })
-
-  #  @friendship = Friendship.new
-  #  friendships = Friendship.where(user: current_user)
-  #  @friends = []
-  #  friendships.each do |friendship|
-  #   @friends << User.find(friendship.friend_id)
-  #  end
   end
-  #  def search
-  #   @user = User.search_by_name(params[:email])
-  # end
+  
 end
