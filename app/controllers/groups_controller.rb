5918f4c503e19a7806a7ff015a24e049933fdc7d
Tsubasa Yamauchi <tsubasa.yamauchi1102@gmail.com>
2021-12-01 12:32:25 -0300

    basic new and create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+
+  def show
+  end
+
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+  def create
+    group = Group.new(group_params)
+    group.user = current_user
+    if group.save
+      redirect_to group_path(group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require('group').permit(:name)
+  end
 end
