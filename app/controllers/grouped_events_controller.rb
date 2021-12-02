d218c4a20f45f3da390b0b465f3ceea5e4fc7556
Tsubasa Yamauchi <tsubasa.yamauchi1102@gmail.com>
2021-12-02 12:43:57 -0300

    votes page created and the function to vote made

--- app/controllers/grouped_events_controller.rb
+++ app/controllers/grouped_events_controller.rb
@@ -1,8 +1,21 @@
 class GroupedEventsController < ApplicationController
-  def index
+  before_action :set_group_grouped_event, only: %i[index votes]
+
+  def index; end
+
+  def votes; end
+
+  def add_vote
+    grouped_event = GroupedEvent.find(params[:grouped_event_id])
+    grouped_event.votes += 1
+    grouped_event.save
+    redirect_to votes_group_grouped_events_path(grouped_event.group)
+  end
+
+  private
+
+  def set_group_grouped_event
     @group = Group.find(params[:group_id])
     @grouped_events = GroupedEvent.where({ group_id: params[:group_id] })
   end
-
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :events, only: %i[index show]
   resources :groups, only: %i[index show new create destroy] do
-    resources :grouped_events, only: %i[index new create destroy]
+    resources :grouped_events, only: %i[index new create destroy] do
+      collection do
+        get 'votes'
+        post 'votes', to: "grouped_events#add_vote"
+      end
+    end
   end
 
-  resource :profiles, only: %i[show] 
+  resource :profiles, only: %i[show]
   resources :friendships, only: %i[new create]
 end
