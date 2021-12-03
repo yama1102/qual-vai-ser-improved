b6734977b2029d77fa6d60f83dabf4522f70a4bd
victor augusto <victorfonsecass@outlook.com>
2021-12-03 00:54:59 -0300

    finalziado

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,23 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :current_event, only: %i[toggle_event_favorites show]
   def index
     @events = Event.all
   end
 
-  def show
+  def toggle_favorite
+    favorite_exist = Favorite.where(event: @event, user: current_user)
+    if favorite_exist[0].nil?
+      favorite = Favorite.new(params[:event])
+      favorite.user = current_user
+      favorite.save
+      redirect_to profiles_path
+    else
+      Favorite.destroy(favorite_exist)
+    end
+  end
+
+  def current_event
     @event = Event.find(params[:id])
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
 
   def show
    @user = current_user
-   @friendships = Friendship.where({ user_id: current_user.id })
+   @friendships = Friendship.where(user_id: current_user.id)
   end
 end
