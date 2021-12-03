18a77902b33b081fea143450d6d0ecf64b807880
victor augusto <victorfonsecass@outlook.com>
2021-12-03 14:37:47 -0300

    todas as páginas conversam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   add_flash_types :alert
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || profiles_path
+  end
 end
