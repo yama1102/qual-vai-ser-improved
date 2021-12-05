cdf47383a9d21fd400c7b5984d01ecc558b7f73d
victorfonsecass <72159465+victorfonsecass@users.noreply.github.com>
2021-12-05 14:56:09 -0300

    Update events_controller.rb
    Retirei o espaço  em before_action do controller.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
-  before_action :current_event, only: %i[ toggle_favorite show]
+  before_action :current_event, only: %i[toggle_favorite show]
 
   def index
     @events = Event.all
