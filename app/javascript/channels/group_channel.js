4a066bcb9a8e3d436052f7b59c1f76ed9931794a
yama1102 <58173316+yama1102@users.noreply.github.com>
2021-12-06 16:26:58 -0300

    Update group_channel.js
    comentarios apagados

--- app/javascript/channels/group_channel.js
+++ app/javascript/channels/group_channel.js
@@ -4,11 +4,9 @@ const initGroupCable = () => {
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
     const id = messagesContainer.dataset.groupId;
-    // console.log(id)
     consumer.subscriptions.create({ channel: "GroupChannel", id: id },
     {
       received(data) {
-        // console.log(data);
         messagesContainer.insertAdjacentHTML('beforeend', data);
       },
     });
