4e35d1f668a6f8462d65bb0aba828d1d2eaceea0
Tsubasa Yamauchi <tsubasa.yamauchi1102@gmail.com>
2021-12-22 17:17:52 -0300

    delete friend buton created

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,6 @@
-
 class FriendshipsController < ApplicationController
-
   def new
-    if params[:nickname].empty?
+    if params[:nickname].nil?
       @users = User.where.not(id: current_user)
     else
       @users = User.search_by_nickname(params[:nickname])
@@ -15,6 +13,12 @@ class FriendshipsController < ApplicationController
     friendship.user = current_user
     friendship.friend_id = params[:friendship][:user_id]
     friendship.save
+    redirect_to new_friendship_path
+  end
+
+  def destroy
+    friend = Friendship.where({user: current_user, friend: params[:id]})
+    friend[0].destroy
     redirect_to new_friendship_path
   end
 
